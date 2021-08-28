1ea6678dedc2aead250592a86318e8aff861114f
petr Soldatov <aulerok@gmail.com>
2021-08-28 23:04:50 +0300

    module15 Json files code

---
+++ 15/2.py
@@ -0,0 +1,32 @@
+import json
+
+template = {
+    'firstname': 'Иван',
+    'lastname': 'Иванов',
+    'isAlive': True,
+    'age': 32,
+    'address': {
+        'streetAddress': 'Нейбута 32',
+        'city': 'Владивосток',
+        'state': '',
+        'postalcode': ''
+    },
+    'phoneNumbers': [
+        {
+            'type': 'mob',
+            'number': '123-333-4455'
+        },
+        {
+            'type': 'office',
+            'number': '123 111-4567'
+        }
+    ],
+    'children': [],
+    'spouse': None
+}
+
+with open('to_json_example.json', 'w', encoding='utf8') as f:
+    json.dump(template, f, ensure_ascii=False, indent=4)
+
+with open('to_json_example.json', encoding='utf8') as f:
+    print(f.read())
